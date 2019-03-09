2b3db3b30ed6fb1c235d0d26a1a659353071a371
Margo <mnchek@mail.ru>
2019-03-10 01:56:04 +0300

    add tests

--- lw2/process_vector_test/process_vector_test.cpp
+++ lw2/process_vector_test/process_vector_test.cpp
@@ -10,7 +10,6 @@ using namespace std;
 //	REQUIRE(2 == GetSymbol(1));
 // }
 
-
 bool IsEqually(const vector<double> &input, const vector<double> &output)
 {
 	return input == output;
@@ -23,12 +22,11 @@ TEST_CASE("input - empty vector, output - empty vector")
 	CHECK(IsEqually(numbers, {}));
 }
 
-
 TEST_CASE("all elements are multiplied by the minimum")
 {
 	vector<double> numbers = { 10, 15, 13 };
 	ProcessVector(numbers);
-	CHECK(IsEqually(numbers, { 100, 130, 150 }));
+	CHECK(IsEqually(numbers, { 100, 150, 130 }));
 }
 
 
@@ -44,5 +42,7 @@ TEST_CASE("all elements are multiplied by the minimum(double elements)")
 {
 	vector<double> numbers = { -3.1, 2.1, 3.7 };
 	ProcessVector(numbers);
-	CHECK(IsEqually(numbers, { (3.7*(-3.1)), (2.1*(-3.1)), (-3.1*(-3.1))  }));
+	CHECK(IsEqually(numbers, { (-3.1*(-3.1)), (2.1*(-3.1)), (3.7*(-3.1)) }));
 }
+
+
