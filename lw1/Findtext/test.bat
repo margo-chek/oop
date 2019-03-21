15f84e86047df2f66abb543162b04f06518ef2c7
Margo <mnchek@mail.ru>
2019-03-21 21:23:00 +0300

    fix test

--- lw1/Findtext/test.bat
+++ lw1/Findtext/test.bat
@@ -1,3 +1,4 @@
+rem test.bat "$(TargetPath)"
 rem %1 - значение первого аргумента командной строки bat-файла (какой он есть)
 rem %~1 - значение первого аргумента командной строки bat-файла с удалением обрамляющих кавычек (если они были)
 
@@ -6,50 +7,50 @@ set PROGRAM="%~1"
 
 rem При запуске без параметров ожидается ненулевой код возврата
 echo test1
-%PROGRAM% >nul
+%PROGRAM% > nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При неудачном открытии файла ожидается ненулевой код возврата
 echo test2
-%PROGRAM% fileNotFound.txt "search line" >nul
+%PROGRAM% fileNotFound.txt "search line" > nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата (строка найдена)
 echo test3
-%PROGRAM% test-data\test.txt "need" > nul
+%PROGRAM% tests\test.txt "need" > nul
 if ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата (строка не найдена)
 echo test4
-%PROGRAM% test-data\test.txt "cat" > nul
+%PROGRAM% tests\test.txt "cat" > nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата (исходный и результирующий файлы совпадают)
 echo test5
-%PROGRAM% test-data\inputFile.txt "need" > "%TEMP%\result.txt" 
-fc.exe "%TEMP%\result.txt" test-data\resultAfterSearchNeed.txt > nul
+%PROGRAM% tests\inputFile.txt "need" > "%TEMP%\result.txt" 
+fc.exe "%TEMP%\result.txt" tests\resultAfterSearchNeed.txt > nul
 if ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается ненулевой код возврата (исходный и результирующий файлы не совпадают)
 echo test6
-%PROGRAM% test-data\inputFile.txt "hamster" > "%TEMP%\result.txt" 
-fc.exe "%TEMP%\result.txt" test-data\resultAfterSearchNeed.txt > nul
+%PROGRAM% tests\inputFile.txt "hamster" > "%TEMP%\result.txt" 
+fc.exe "%TEMP%\result.txt" tests\resultAfterSearchNeed.txt > nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается ненулевой код возврата (разный регистр строк)
 echo test7
-%PROGRAM% test-data\inputFile.txt "Need" > "%TEMP%\result.txt" 
-fc.exe "%TEMP%\result.txt" test-data\resultAfterSearchNeed.txt > nul
+%PROGRAM% tests\inputFile.txt "Need" > "%TEMP%\result.txt" 
+fc.exe "%TEMP%\result.txt" tests\resultAfterSearchNeed.txt > nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается ненулевой код возврата (пустой файл)
 echo test8
-%PROGRAM% test-data\emptyFile.txt "search line" >nul
+%PROGRAM% tests\emptyFile.txt "search line" >nul
 if NOT ERRORLEVEL 1 goto err
 
 rem При запуске с правильными параметрами ожидается ненулевой код возврата (пустая строка)
 echo test9
-%PROGRAM% test-data\inputFile.txt "" >nul
+%PROGRAM% tests\inputFile.txt "" >nul
 if NOT ERRORLEVEL 1 goto err
 
 echo OK
