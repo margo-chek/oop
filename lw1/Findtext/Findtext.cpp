0f0f1d8397c841555637d8712bc2cd60cb267bcc
Margo <mnchek@mail.ru>
2019-04-14 20:14:03 +0300

    issue fix Findtext

--- lw1/Findtext/Findtext.cpp
+++ lw1/Findtext/Findtext.cpp
@@ -14,43 +14,39 @@ void PrintLineNumbersContainingString(ostream& output, const vector<size_t>& num
 }
 
 
-//optional<Vec> SearchTextInFile(const string& inputFile, const string& searchString, vector<size_t>& numberString) - надо выход?
-optional<Vec> SearchTextInFile(const string& inputFile, const string& searchString)
+std::optional<Vec> SearchTextInFile(const string& inputFile, const string& searchString)
 {
-	ifstream file;
-	file.open(inputFile);
-	string line;
-	size_t lineNumber = 0;
-	bool textWasFound = false;
-	Vec numberString = {};
+	ifstream file(inputFile); // вместо ifstream file; 	file.open(inputFile);
 
 	if (!file.is_open())
 	{
 		cerr << "File not found" << endl;
-		return {};
+		return nullopt;
 	}
 
 	if (searchString.empty())
 	{
 		cerr << "Searching string must be not empty" << endl;
-		return {};
+		return nullopt;
 	}
+
+	string line;
+	size_t lineNumber = 0;
+	Vec numberString;
 
 	if (line.find(searchString) == string::npos)
 	{
 		cerr << "Text not found" << endl;
-		return {};
+		return nullopt;
 	}
 
-	while (getline(inputFile, line))
+	while (getline(file, line))
 	{
 		++lineNumber;
 
 		if (line.find(searchString) != string::npos)
 		{
-			// vector<size_t> numberString(istream_iterator<size_t>(lineNumber), (istream_iterator<size_t>())); - не знаю, как надо
 			numberString.push_back(lineNumber);
-			textWasFound = true;
 		}
 	}
 
@@ -64,7 +60,7 @@ optional<Args> ParseCmdLineArgs(const int argc, char* argv[])
 	{
 		cerr << "Invalid argument count\n"
 			 << "Usage: findtext.exe <inputFile> <searchString>" << endl;
-		return {}; // return null_opt;
+		return nullopt;
 	}
 	argsValue = make_pair(argv[1], argv[2]);
 
@@ -78,14 +74,13 @@ int main(int argc, char* argv[])
 	if (!cmdLineArgs.has_value()) // Метод has_value класса std::optional, говорит, есть ли там значение. Возвращает булевое значение.
 		return 1;
 
-	// Args args = cmdLineArgs.value(); // Чтобы получить доступ к значению, можно использовать метод value
+	// Чтобы получить доступ к значению, можно использовать метод value
+	 optional<Vec> matchesCount = SearchTextInFile(cmdLineArgs.value().first, cmdLineArgs.value().second);
 
-	 optional<Vec> matchesCount = SearchTextInFile(cmdLineArgs.first, cmdLineArgs.second);
+	 if (!matchesCount.has_value())
+		 return 1;
 
-	 /*if (!matchesCount.has_value())
-		 return 1;*/
-
-	 PrintLineNumbersContainingString(matchesCount);
+	 PrintLineNumbersContainingString(cout, matchesCount.value());
 
 	return 0;
 }
